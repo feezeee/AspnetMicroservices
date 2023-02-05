594e04f43e6a18693ebfc69abd1a3f654dded170
Денис Скурат <den.skur2016@yandex.ru>
2023-02-05 19:25:36 +0300

    Implement basket api microservice

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public ICollection<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart() : this(string.Empty)
+        {
+            
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice => Items.Sum(item => item.Quantity * item.Price);
+    }
+}
